39362453b93a912f1c30ddd3a0962b09ab830ad0
Danila <62538561+GD-alt@users.noreply.github.com>
2023-09-06 00:32:37 +0300

    Testpage added

--- src/templates/script.js
+++ src/templates/script.js
@@ -1,22 +1,72 @@
 const element = document.querySelector('.icon');
 const bolds = document.querySelectorAll('b');
+const italics = document.querySelectorAll('i');
+const underline = document.querySelectorAll('u');
+const strikethrough = document.querySelectorAll('s');
+const marked = document.querySelectorAll('mark');
+const links = document.querySelectorAll('a');
 const h1 = document.querySelector('h1');
 
 element.addEventListener('click', () => {
   const randomColor = getRandomColor();
   element.style.color = randomColor;
+
   bolds.forEach(bold => {
     bold.style.color = randomColor;
   });
+
+  italics.forEach(italic => {
+    italic.style.color = randomColor;
+  });
+
+  underline.forEach(underline => {
+    underline.style.color = randomColor;
+  });
+
+  strikethrough.forEach(strikethrough => {
+    strikethrough.style.color = randomColor;
+  });
+
+  marked.forEach(marked => {
+    marked.style.backgroundColor = randomColor;
+  });
+
+  links.forEach(link => {
+    link.style.color = randomColor;
+  });
+
   h1.style.color = randomColor;
 });
 
 document.addEventListener('DOMContentLoaded', () => {
     const randomColor = getRandomColor();
+
     element.style.color = randomColor;
+
     bolds.forEach(bold => {
         bold.style.color = randomColor;
-      });
+    });
+
+    italics.forEach(italic => {
+        italic.style.color = randomColor;
+    });
+
+    underline.forEach(underline => {
+        underline.style.color = randomColor;
+    });
+
+    strikethrough.forEach(strikethrough => {
+        strikethrough.style.color = randomColor;
+    });
+
+    marked.forEach(marked => {
+        marked.style.backgroundColor = randomColor;
+    });
+
+    links.forEach(link => {
+        link.style.color = randomColor;
+    });
+
     h1.style.color = randomColor;
   });
 
