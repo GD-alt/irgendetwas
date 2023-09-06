2bfd2015fcfbcb467de9bc74a9a271a5b0613aef
Danila <62538561+GD-alt@users.noreply.github.com>
2023-09-07 00:17:51 +0300

    Stylish as f

--- src/templates/script.js
+++ src/templates/script.js
@@ -6,9 +6,13 @@ const strikethrough = document.querySelectorAll('s');
 const marked = document.querySelectorAll('mark');
 const links = document.querySelectorAll('a');
 const h1 = document.querySelector('h1');
+const buttons = document.querySelectorAll('button');
+const tableHeaders = document.querySelectorAll('th');
+
+var randomColor = getRandomColor();
 
 element.addEventListener('click', () => {
-  const randomColor = getRandomColor();
+  randomColor = getRandomColor();
   element.style.color = randomColor;
 
   bolds.forEach(bold => {
@@ -35,11 +39,31 @@ element.addEventListener('click', () => {
     link.style.color = randomColor;
   });
 
+  tableHeaders.forEach(tableHeader => {
+    tableHeader.style.backgroundColor = randomColor;
+    tableHeader.style.color = '#0d0d0d';
+    tableHeader.style.border = '1px solid ' + randomColor;
+  });
+
   h1.style.color = randomColor;
 });
 
+buttons.forEach(button => {
+  button.addEventListener('mouseover', () => {
+    button.style.backgroundColor = randomColor;
+    button.style.color = '#0d0d0d';
+    button.style.border = '1px solid ' + randomColor;
+  });
+
+  button.addEventListener('mouseout', () => {
+    button.style.backgroundColor = '#0d0d0d';
+    button.style.color = '#e1e1e1';
+    button.style.border = '1px solid #e1e1e1';
+  });
+});
+
 document.addEventListener('DOMContentLoaded', () => {
-    const randomColor = getRandomColor();
+    randomColor = getRandomColor();
 
     element.style.color = randomColor;
 
@@ -67,8 +91,24 @@ document.addEventListener('DOMContentLoaded', () => {
         link.style.color = randomColor;
     });
 
+    tableHeaders.forEach(tableHeader => {
+      tableHeader.style.backgroundColor = randomColor;
+      tableHeader.style.color = '#0d0d0d';
+      tableHeader.style.border = '1px solid ' + randomColor;
+    });
+
     h1.style.color = randomColor;
   });
+
+window.addEventListener('scroll', function() {
+  var scrolled = window.scrollY > 0;
+  var element = document.querySelector('.header');
+  if (scrolled) {
+    element.classList.add('shadow-header');
+  } else {
+    element.classList.remove('shadow-header');
+  }
+});
 
 function getRandomColor() {
   const letters = '0123456789ABCDEF';
